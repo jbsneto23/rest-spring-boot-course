38c0627f256c7e2ef1d16c3217f8340b37add333
João Batista de Souza Neto <jbsneto23@gmail.com>
2022-07-08 18:59:52 -0300

    section 12 - Modifying JSON properties

--- rest-spring-boot-course/src/main/java/br/com/jbsneto/data/dto/v1/PersonDTO.java
+++ rest-spring-boot-course/src/main/java/br/com/jbsneto/data/dto/v1/PersonDTO.java
@@ -1,12 +1,23 @@
 package br.com.jbsneto.data.dto.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 @Data
+@JsonPropertyOrder({"id", "firstName", "lastName", "gender", "address"})
 public class PersonDTO {
     private Long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
+
     private String address;
+
+    @JsonIgnore
     private String gender;
 }
