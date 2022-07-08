c85ea5471c881a9a99fd4725a47ba841938b8d42
João Batista de Souza Neto <jbsneto23@gmail.com>
2022-07-08 18:52:33 -0300

    section 11 - Adding database migration with Flyway

---
+++ rest-spring-boot-course/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1,'Natal','João Batista','Male','de Souza Neto'),
+    (2,'Upanema','Maria','Female','José'),
+    (3,'Assu','João','Male','Junior'),
+    (4,'São José dos Campos','Carol','Female','Souza');
